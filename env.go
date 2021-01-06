a8690240c618416c28cca2e845fd6bb68c3e8f25
billcoding <wangmiao07@sina.com>
2021-01-06 18:50:57 +0800

    Add env vars

--- env.go
+++ env.go
@@ -1,13 +1,13 @@
 package flygo
 
 import (
+	"github.com/billcoding/calls"
 	"os"
 	"strconv"
-	"time"
 )
 
 const (
-	flggoConfig = "FLYGO_CONFIG" //env config file
+	flygoConfig = "FLYGO_CONFIG" //env config file
 
 	flygoDevDebug = "FLYGO_DEV_DEBUG" //env dev debug
 
@@ -17,35 +17,48 @@ const (
 	flygoBannerEnable = "FLYGO_BANNER_ENABLE" //env banner enable
 	flygoBannerType   = "FLYGO_BANNER_TYPE"   //env banner type
 	flygoBannerText   = "FLYGO_BANNER_TEXT"   //env banner text
-	flygoBannerFile   = "FLYGO_BANNER_ENABLE" //env banner file
+	flygoBannerFile   = "FLYGO_BANNER_FILE"   //env banner file
 
 	flygoServerTlsEnable   = "FLYGO_SERVER_TLS_ENABLE"    //env server tls enable
 	flygoServerTlsCertFile = "FLYGO_SERVER_TLS_CERT_FILE" //env server tls cert file
 	flygoServerTlsKeyFile  = "FLYGO_SERVER_TLS_KEY_FILE"  //env server tls key file
+
+	flygoTemplateEnable = "FLYGO_TEMPLATE_ENABLE" //env template enable
+	flygoTemplateCache  = "FLYGO_TEMPLATE_CACHE"  //env template cache
+	flygoTemplateRoot   = "FLYGO_TEMPLATE_ROOT"   //env template root
+	flygoTemplateSuffix = "FLYGO_TEMPLATE_SUFFIX" //env template suffix
 )
 
 //set config
 func (a *App) setConfig() {
-	config := stringEnv(flggoConfig)
-	if config != "" {
+	config := stringEnv(flygoConfig)
+	calls.NEmpty(config, func() {
 		a.ConfigFile = config
-	}
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoConfig, config)
+		})
+	})
 }
 
 //set dev debug
 func (a *App) setDevDebug() {
-	debug := stringEnv(flygoDevDebug)
-	if debug != "" {
-		a.Config.Dev.Debug = boolEnv(debug)
-	}
+	calls.NEmpty(stringEnv(flygoDevDebug), func() {
+		a.Config.Dev.Debug = boolEnv(flygoDevDebug)
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoDevDebug, a.Config.Dev.Debug)
+		})
+	})
 }
 
 //set server host
 func (a *App) setServerHost() {
 	host := stringEnv(flygoServerHost)
-	if host != "" {
+	calls.NEmpty(host, func() {
 		a.Config.Server.Host = host
-	}
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoServerHost, host)
+		})
+	})
 }
 
 //set server port
@@ -53,15 +66,20 @@ func (a *App) setServerPort() {
 	port, err := intEnv(flygoServerPort)
 	if err == nil {
 		a.Config.Server.Port = port
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoServerPort, a.Config.Server.Port)
+		})
 	}
 }
 
 //set banner enable
 func (a *App) setBannerEnable() {
-	bannerEnable := stringEnv(flygoBannerEnable)
-	if bannerEnable != "" {
+	calls.NEmpty(stringEnv(flygoBannerEnable), func() {
 		a.Config.Banner.Enable = boolEnv(flygoBannerEnable)
-	}
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoBannerEnable, a.Config.Banner.Enable)
+		})
+	})
 }
 
 //set banner type
@@ -69,13 +87,20 @@ func (a *App) setBannerType() {
 	bannerType := stringEnv(flygoBannerType)
 	if bannerType != "" {
 		a.Config.Banner.Type = bannerType
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoBannerType, bannerType)
+		})
 	}
 }
 
 //set banner text
 func (a *App) setBannerText() {
-	if stringEnv(flygoBannerText) != "" {
-		a.Config.Banner.Text = stringEnv(flygoBannerText)
+	bannerText := stringEnv(flygoBannerText)
+	if bannerText != "" {
+		a.Config.Banner.Text = bannerText
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoBannerText, bannerText)
+		})
 	}
 }
 
@@ -84,15 +109,20 @@ func (a *App) setBannerFile() {
 	bannerFile := stringEnv(flygoBannerFile)
 	if bannerFile != "" {
 		a.Config.Banner.File = bannerFile
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoBannerFile, bannerFile)
+		})
 	}
 }
 
 //set server tls enable
 func (a *App) setServerTlsEnable() {
-	serverTlsEnable := stringEnv(flygoServerTlsEnable)
-	if serverTlsEnable != "" {
+	calls.NEmpty(stringEnv(flygoServerTlsEnable), func() {
 		a.Config.Server.TLS.Enable = boolEnv(flygoServerTlsEnable)
-	}
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoServerTlsEnable, a.Config.Server.TLS.Enable)
+		})
+	})
 }
 
 //set server tls cert file
@@ -100,6 +130,9 @@ func (a *App) setServerTlsCertFile() {
 	serverTlsCertFile := stringEnv(flygoServerTlsCertFile)
 	if serverTlsCertFile != "" {
 		a.Config.Server.TLS.CertFile = serverTlsCertFile
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoServerTlsCertFile, serverTlsCertFile)
+		})
 	}
 }
 
@@ -108,7 +141,52 @@ func (a *App) setServerTlsKeyFile() {
 	serverTlsKeyFile := stringEnv(flygoServerTlsKeyFile)
 	if serverTlsKeyFile != "" {
 		a.Config.Server.TLS.KeyFile = serverTlsKeyFile
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoServerTlsKeyFile, serverTlsKeyFile)
+		})
 	}
+}
+
+//set template enable
+func (a *App) setTemplateEnable() {
+	calls.NEmpty(stringEnv(flygoTemplateEnable), func() {
+		a.Config.Template.Enable = boolEnv(flygoTemplateEnable)
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoTemplateEnable, a.Config.Template.Enable)
+		})
+	})
+}
+
+//set template cache
+func (a *App) setTemplateCache() {
+	calls.NEmpty(stringEnv(flygoTemplateCache), func() {
+		a.Config.Template.Cache = boolEnv(flygoTemplateCache)
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoTemplateCache, a.Config.Template.Cache)
+		})
+	})
+}
+
+//set template root
+func (a *App) setTemplateRoot() {
+	templateRoot := stringEnv(flygoTemplateRoot)
+	calls.NEmpty(templateRoot, func() {
+		a.Config.Template.Root = templateRoot
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoTemplateRoot, templateRoot)
+		})
+	})
+}
+
+//set template suffix
+func (a *App) setTemplateSuffix() {
+	templateSuffix := stringEnv(flygoTemplateSuffix)
+	calls.NEmpty(templateSuffix, func() {
+		a.Config.Template.Suffix = templateSuffix
+		a.DebugTrace(func() {
+			a.Logger.Info("[Env]Set [%v] = [%v]", flygoTemplateSuffix, templateSuffix)
+		})
+	})
 }
 
 //parseEnv
@@ -130,6 +208,15 @@ func (a *App) parseEnv() {
 	//set server port
 	a.setServerPort()
 
+	//set server tls enable
+	a.setServerTlsEnable()
+
+	//set server tls cert file
+	a.setServerTlsCertFile()
+
+	//set server tls key file
+	a.setServerTlsKeyFile()
+
 	//set banner enable
 	a.setBannerEnable()
 
@@ -142,14 +229,17 @@ func (a *App) parseEnv() {
 	//set banner file
 	a.setBannerFile()
 
-	//set server tls enable
-	a.setServerTlsEnable()
+	//set template enable
+	a.setTemplateEnable()
 
-	//set server tls cert file
-	a.setServerTlsCertFile()
+	//set template cache
+	a.setTemplateCache()
 
-	//set server tls key file
-	a.setServerTlsKeyFile()
+	//set template root
+	a.setTemplateRoot()
+
+	//set template suffix
+	a.setTemplateSuffix()
 }
 
 //stringEnv
@@ -160,20 +250,14 @@ func stringEnv(key string) string {
 //boolEnv
 func boolEnv(key string) bool {
 	be := os.Getenv(key)
-	if be == "" {
-		return false
-	}
-	return be == "ON" || be == "on" || be == "1" || be == "true" || be == "TRUE"
+	//	case "1", "t", "T", "true", "TRUE", "True":
+	//	case "0", "f", "F", "false", "FALSE", "False":
+	b, err := strconv.ParseBool(be)
+	return err == nil && b
 }
 
 //intEnv
 func intEnv(key string) (int, error) {
 	be := os.Getenv(key)
 	return strconv.Atoi(be)
-}
-
-//durationEnv
-func durationEnv(key string) (time.Duration, error) {
-	be := os.Getenv(key)
-	return time.ParseDuration(be)
 }
